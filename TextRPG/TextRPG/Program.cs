437acb60da42cb5e6932cf8cf84531321925c45a
GYALLERHORN <kjins1998@gmail.com>
2023-08-21 18:56:15 +0900

    인벤토리 표시창 작성 완료
    일단.. 창 표시는 된다.
    아이템 장비 여부 표시, 플레이어 스탯 갱신 이 문제가 아니라
    아이템 선택부터 일단 해야 되는데 대체 어떻게 하지?

--- TextRPG/TextRPG/Program.cs
+++ TextRPG/TextRPG/Program.cs
@@ -9,7 +9,7 @@ internal class Text_rpg
         private static PlayerInventory inventory;
 
 
-        class RPGClass
+        class WelcomeScene
         {
             static void SetStatus()
             {
@@ -17,13 +17,16 @@ internal class Text_rpg
             }
             static void SetInventory()
             {
-                
+
             }
 
             static void Main(string[] args)
             {
                 SetStatus();
                 PrimaryScene primaryScene = new PrimaryScene();
+
+                Console.Write("우선, 이름을 입력하세요 : ");
+                status.Name = Console.ReadLine();
                 Console.WriteLine("소개글");
                 Console.WriteLine("시작하려면 아무 키나 누르세요. 0을 누르면 종료합니다.");
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
@@ -43,6 +46,8 @@ internal class Text_rpg
         {
             public void PrimaryChoice()
             {
+                SecondaryScene secondaryScene = new SecondaryScene();
+
                 Console.WriteLine("던전 입장 전 상태를 확인하고, 정비하세요. 선택지의 번호를 입력하면 해당 기능이 실행됩니다.");
                 Console.WriteLine("\t[1] : 내 현재 상태");
                 Console.WriteLine("\t[2] : 내 장비");
@@ -50,7 +55,7 @@ internal class Text_rpg
                 Console.WriteLine();
                 Console.WriteLine("\t[0] : 게임 종료\n");
 
-                
+
                 Console.WriteLine("숫자를 입력하세요.(0~2) : ");
                 int input = CheckAvailable(0, 2);
                 switch (input)
@@ -59,10 +64,10 @@ internal class Text_rpg
                         Console.WriteLine("게임을 종료합니다.");
                         break;
                     case 1:
-                        ShowStatus();
+                        secondaryScene.ShowStatus();
                         break;
                     case 2:
-                        Console.WriteLine(input + " 선택");
+                        secondaryScene.ShowInventory();
                         break;
                 }
 
@@ -82,26 +87,46 @@ internal class Text_rpg
                         Console.WriteLine("올바른 숫자를 입력하세요");
                     }
                 }
-
-                void ShowStatus()
-                {
-                    Console.WriteLine("현재 상태입니다.");
-                    Console.WriteLine($"NAME : {status.Name}");
-                    Console.WriteLine($"LEVEL : {status.Level}");
-                    Console.WriteLine($"HP : {status.Hp}");
-                    Console.WriteLine($"Atk : {status.Atk}");
-                    Console.WriteLine($"DEF : {status.Def}");
-                }
             }
+        }
+
+        public class SecondaryScene
+        {
+            public void ShowStatus()
+            {
+                Console.Clear();
+                Console.WriteLine("현재 상태입니다.");
+                Console.WriteLine($"NAME  : {status.Name}");
+                Console.WriteLine($"LEVEL : {status.Level}");
+                Console.WriteLine($"HP    : {status.Hp}");
+                Console.WriteLine($"Atk   : {status.Atk}");
+                Console.WriteLine($"DEF   : {status.Def}");
+            }
+
+            PlayerInventory inventory = new PlayerInventory();
+
+            public void ShowInventory()
+            {
+                Item weapon1 = new Item("나뭇가지".PadLeft(20), 3, "굵은 나뭇가지입니다.");
+                Item armor1 = new Item("가죽갑옷".PadLeft(20), 5, "ㅇㅇ알돌ㄴㄴㄴ 갑옷입니다.");
+                inventory.additem(weapon1);
+                inventory.additem(armor1);
+
+                Console.WriteLine("소유 중인 아이템 목록입니다.");
+                inventory.DisplayInventory();
+            }
+
+            
+
         }
 
         public class PlayerStatus
         {
-            public string Name { get; }
+            public string Name { get; set; }
             public int Level { get; }
             public int Hp { get; }
             public int Atk { get; }
-            public int Def { get;  }
+            public int Def { get; }
 
             public PlayerStatus(string name, int level, int hp, int atk, int def)
             {
@@ -137,10 +162,19 @@ internal class Text_rpg
             {
                 items = new List<Item>();
             }
-            //public void AddItem(Item item)
-            //{
-            //    items.Add(item);
-            //}
+
+            public void additem(Item item)
+            {
+                items.Add(item);
+            }
+
+            public void DisplayInventory()
+            {
+                foreach (Item item in items)
+                {
+                    Console.WriteLine($"{item.ItemName}, {item.Effect}, {item.Description}");
+                }
+            }
         }
 
     }
