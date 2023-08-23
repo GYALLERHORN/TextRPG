e6b0aafb5c65c26b894c460887291ee2899c7afa
GYALLERHORN <kjins1998@gmail.com>
2023-08-23 14:31:43 +0900

    커밋합니다

--- TextRPG/TextRPG/Program.cs
+++ TextRPG/TextRPG/Program.cs
@@ -2,13 +2,13 @@
 using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Channels;
+using static Text_rpg.InGame;
 
 internal class Text_rpg
 {
     public class InGame
     {
         private static Player player; // PlayerStatus라는 형식(클래스)의 status변수 선언
-        // static클래스가 아니라 뭐다? static한 메서드다.
 
         class WelcomeScene
         {
@@ -49,12 +49,13 @@ internal class Text_rpg
                 Console.WriteLine("던전 입장 전 상태를 확인하고, 정비하세요. 선택지의 번호를 입력하면 해당 기능이 실행됩니다.");
                 Console.WriteLine("\t[1] : 내 현재 상태");
                 Console.WriteLine("\t[2] : 내 장비");
+                Console.WriteLine("\t[3] : 아직 구현 안한 상점");
                 Console.WriteLine();
                 Console.WriteLine();
                 Console.WriteLine("\t[0] : 게임 종료\n");
-                Console.WriteLine("숫자를 입력하세요.(0~2) : ");
+                Console.WriteLine("숫자를 입력하세요.(0~3) : ");
 
-                int input = inputCheck.CheckAvailable(0, 2);
+                int input = inputCheck.CheckAvailable(0, 3);
                 switch (input)
                 {
                     case 0:
@@ -65,6 +66,9 @@ internal class Text_rpg
                         break;
                     case 2:
                         secondaryScene.ShowInventory();
+                        break;
+                    case 3:
+                        secondaryScene.ShowStore();
                         break;
                 }
 
@@ -106,11 +110,12 @@ internal class Text_rpg
                 Console.WriteLine($"ATK   : {showATK}");
                 string showDEF = player.isDEFBuffed ? player.buffedPlayerDEF : player.Def.ToString();
                 Console.WriteLine($"DEF   : {showDEF}");
-                Console.WriteLine($"NAME  : {player.Gold}");
+                Console.WriteLine($"GOLD  : {player.Gold}");
                 Console.WriteLine("=========================");
                 Console.Write("첫 화면으로 나가려면 0을 입력하세요 : ");
-            InputCheckMachine inputCheck = new InputCheckMachine();
-                int input = inputCheck.CheckAvailable(0,0);
+
+                InputCheckMachine inputCheck = new InputCheckMachine();
+                int input = inputCheck.CheckAvailable(0, 0);
                 if (input == 0)
                 {
                     PrimaryScene.PrimaryChoice();
@@ -118,19 +123,36 @@ internal class Text_rpg
             }
 
             PlayerInventory inventory = new PlayerInventory();
-            bool isFirstEntry = true;
+            bool isFirstInvenEntry = true;
             public void ShowInventory()
             {
-                if (isFirstEntry)
+                if (isFirstInvenEntry)
                 {
-                    Item item1 = new Item("    ", "나뭇가지", "ATK +2", "굵은 나뭇가지입니다.");
-                    inventory.additem(item1);
+                    Item item1 = new Item("    ", "직검", "ATK +2", "평범한 양손 직검입니다.");
+                    inventory.Additem(item1);
                     Item item2 = new Item("    ", "가죽갑옷", "DEF +3", "가죽제 갑옷입니다.");
-                    inventory.additem(item2);
-                    isFirstEntry = false;
+                    inventory.Additem(item2);
+                    isFirstInvenEntry = false;
                 }
                 inventory.DisplayInventory();
             }
+
+            Store store = new Store();
+            bool isFirstStoreEntry = true;
+            public void ShowStore()
+            {
+                Console.WriteLine("상점을 차릴겁니다.\n\n");
+                if (isFirstStoreEntry)
+                {
+                    Product product1 = new Product(" ", "아주 무거운 갑옷", "ATK -80", "지금은 못입어요.", 1000);
+                    store.AddProduct(product1);
+                    Product product2 = new Product(" ", "파랗게 빛나는 단검", "ATK +999", "뾰족해져요.", 10);
+                    store.AddProduct(product2);
+                    isFirstStoreEntry = false;
+                }
+                store.DisplayProduct();
+            }
+
         }
 
         public class Player
@@ -143,6 +165,8 @@ internal class Text_rpg
             public int Def { get; set; }
             public int Gold { get; set; }
 
+            public PlayerInventory myInventory;
+
             public Player(string name, string job, int level, int hp, int atk, int def, int gold)
             {
                 Name = name;
@@ -153,7 +177,6 @@ internal class Text_rpg
                 Def = def;
                 Gold = gold;
             }
-
             public void Equip(Item item)
             {
                 string[] itemStatus = item.Effect.Split(" ");
@@ -212,6 +235,15 @@ internal class Text_rpg
                         break;
                 }
             }
+            public void Buy(Product product)
+            {
+                PlayerInventory inventory = new PlayerInventory();
+                player.Gold -= product.Cost;
+                Console.WriteLine("물건을 샀습니다.");
+                Thread.Sleep(300);
+                Item boughtItem = new Item(product.EquippedMark, product.ProductName, product.Effect, product.Description);
+                inventory.Additem(boughtItem);
+            }
         }
 
         public class Item
@@ -233,7 +265,7 @@ internal class Text_rpg
             PlayerInventory inventory = new PlayerInventory();
             public void GetItem(Item item)
             {
-                inventory.additem(item);
+                inventory.Additem(item);
             }
         }
 
@@ -248,12 +280,14 @@ internal class Text_rpg
                 items = new List<Item>();
             }
 
-            public void additem(Item item)
+            public void Additem(Item item)
             {
+                Console.WriteLine($"{item}을 샀습니다.");
+                Thread.Sleep(6000);
                 items.Add(item);
             }
 
-                  InputCheckMachine inputCheck = new InputCheckMachine();
+            InputCheckMachine inputCheck = new InputCheckMachine();
             public void DisplayInventory()
             {
                 while (true)
@@ -284,19 +318,100 @@ internal class Text_rpg
                         Item selecteditem = items[input - 1];
                         if (!selecteditem.IsEquipped)
                         {
-                            Console.WriteLine(selecteditem.ItemName + " 장착");
                             player.Equip(selecteditem);
                         }
                         else
                         {
                             player.Unequip(selecteditem);
                         }
-
                     } 
                 }
                 
             }
         }
 
+        public class Product
+        {
+            public bool IsEquipped { get; set; }
+            public string EquippedMark { get; set; }
+            public string ProductName { get; set; }
+            public string Effect { get; set; }
+            public string Description { get; set; }
+            public int Cost { get; set; }
+
+
+            public Product(string equippedMark, string productName, string effect, string description, int cost)
+            {
+                EquippedMark = equippedMark;
+                ProductName = productName;
+                Effect = effect;
+                Description = description;
+                Cost = cost;
+            }
+
+            Store store = new Store();
+            public void GetProduct(Product product)
+            {
+                store.AddProduct(product);
+            }
+        }
+
+        public class Store
+        {
+            public List<Product> Products { get; set; }
+
+            public Store()
+            {
+                Products = new List<Product>();
+            }
+
+            public void AddProduct(Product product)
+            {
+                Products.Add(product);
+            }
+
+            public void RemoveProduct(Product product)
+            {
+                Products.Remove(product);
+            }
+
+            public void DisplayProduct()
+            {
+                Console.WriteLine("상점입니다.");
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("판매 중인 아이템 목록입니다.");
+                    Console.WriteLine("아이템 번호 |    아이템명    | [E/U] | 장착효과 | 가격");
+                    Console.WriteLine("-------------------------------------------------------------------------");
+
+                    int productNumber = 1;
+                    foreach (Product product in Products)
+                    {
+                        Console.WriteLine($"#{string.Format("{0:        ######00}", productNumber++)} : {product.ProductName.PadLeft(10)} |{product.EquippedMark.PadLeft(4)} | {product.Effect.PadLeft(5)} | {product.Cost}");
+                    }
+                    Console.WriteLine("=========================================================");
+                    Console.Write("숫자 입력으로 구입할 아이템을 선택하세요\n 0 입력 시 첫 화면으로 돌아갑니다. : ");
+                    InputCheckMachine inputCheck = new InputCheckMachine();
+
+                    int input = inputCheck.CheckAvailable(0, Products.Count);
+
+                    if (input == 0)
+                    {
+                        PrimaryScene.PrimaryChoice();
+                        break;
+                    }
+                    else
+                    {
+                        Product selectedProduct = Products[input - 1];
+                        player.Buy(selectedProduct);
+                        Products.Remove(selectedProduct);
+                    }
+                }
+
+
+            }
+        }
     }
 }
